bd94225c4786321c094dc883a5449e473fb14352
Сергей Зеленковский <svzelenkoskii@gmail.com>
2023-01-26 14:25:29 +0300

    добавил маршрут для api_foodgram: path(api/, include(api_foodgram.urls))

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -21,6 +21,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('api_foodgram.urls')),
     path(
         'redoc/',
         TemplateView.as_view(template_name='redoc.html'),
