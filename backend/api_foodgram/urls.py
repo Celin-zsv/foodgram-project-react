0f40c0e3d24606b20c6512a2f31262871077c674
Сергей Зеленковский <svzelenkoskii@gmail.com>
2023-01-26 23:40:28 +0300

    add file api_foodgram/urls.py

---
+++ backend/api_foodgram/urls.py
@@ -0,0 +1,15 @@
+from api_foodgram import views
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+# from .views import views
+
+app_name = 'api_foodgram'
+
+router = DefaultRouter()
+router.register('tag', views.TagViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('zsv2/', views.zsv_page, name='zsv_page'),
+    # path('', views.get_token),
+]
