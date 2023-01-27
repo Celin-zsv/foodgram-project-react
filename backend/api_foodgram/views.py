cc252d525da829fd1a7f4996e3329f22f5be987f
Сергей Зеленковский <svzelenkoskii@gmail.com>
2023-01-27 22:39:16 +0300

    add viewset IngredientViewSet + search_fields
    поиск по полю name: по первому символу/символам, регистронезависим только к латинице (ожидается, что к кирилице будет регистронезависим после переноса на Postgresql)

--- backend/api_foodgram/views.py
+++ backend/api_foodgram/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework import viewsets
-from.serializers import TagSerializer
-from .models import Tag
+from.serializers import TagSerializer, IngredientSerializer
+from .models import Tag, Ingredient
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 
 def zsv_page(request):
@@ -12,3 +14,11 @@ def zsv_page(request):
 class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
+
+
+class IngredientViewSet(viewsets.ModelViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
+    search_fields = ('^name',)
+    # search_fields = ('$name',)
