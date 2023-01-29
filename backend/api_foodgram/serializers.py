de53c3683ea338cc8f8206d8412174cdc6efabbe
Сергей Зеленковский <svzelenkoskii@gmail.com>
2023-01-29 10:06:52 +0300

    add 2 serializers: Ingredient2Serializer, RecipeSerializer

--- backend/api_foodgram/serializers.py
+++ backend/api_foodgram/serializers.py
@@ -1,14 +1,39 @@
 from rest_framework import serializers
-from .models import Tag, Ingredient
+from .models import Tag, Ingredient, Recipe, IngredientRecipe
 
 
 class TagSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Tag
         fields = ('id', 'name', 'color', 'slug',)
 
 
 class IngredientSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Ingredient
         fields = ('id', 'name', 'measurement_unit',)
+
+
+class Ingredient2Serializer(serializers.ModelSerializer):
+    amount = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Ingredient
+        fields = ('id', 'name', 'measurement_unit', 'amount',)
+
+    def get_amount(self, obj):
+        res = IngredientRecipe.objects.filter(ingredient_id=obj.id)
+        return res[0].amount
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    ingredients = Ingredient2Serializer(read_only=True, many=True)
+    tags = TagSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = Recipe
+        fields = (
+            'id', 'name', 'author', 'text',
+            'cooking_time', 'ingredients', 'tags',)
