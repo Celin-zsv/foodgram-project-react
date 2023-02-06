964b0b7140f32ead495803589947281761daff71
Сергей Зеленковский <svzelenkoskii@gmail.com>
2023-02-06 11:15:03 +0300

    add: 1)router users, 2)path <auth/> for djoser token

--- backend/api_foodgram/urls.py
+++ backend/api_foodgram/urls.py
@@ -8,8 +8,10 @@ router = DefaultRouter()
 router.register('tag', views.TagViewSet)
 router.register('ingredient', views.IngredientViewSet)
 router.register('recipes', views.RecipesWriteViewSet)
+router.register('users', views.CustomUserViewSet)  # Работа с пользователями
 
 urlpatterns = [
     path('', include(router.urls)),
     path('zsv2/', views.zsv_page, name='zsv_page'),
+    path('auth/', include('djoser.urls.authtoken')),  # Работа с токенами
 ]
