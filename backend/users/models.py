e46630b4ee7cc0cc34a67846778d460ee9db4f03
Сергей Зеленковский <svzelenkoskii@gmail.com>
2023-02-06 10:47:22 +0300

    add app <users>

---
+++ backend/users/models.py
@@ -0,0 +1,47 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinLengthValidator
+from django.db import models
+
+
+class User(AbstractUser):
+    GUEST = 'guest'
+    USER = 'user'
+    ADMIN = 'admin'
+    ROLES = [
+        (GUEST, 'guest'),
+        (USER, 'user'),
+        (ADMIN, 'admin'),
+    ]
+    email = models.EmailField(
+        unique=True,
+        verbose_name='Почта'
+    )
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        verbose_name='Имя пользователя'
+    )
+    first_name = models.CharField(
+        max_length=150,
+        verbose_name='Имя',
+        default='not knowen'
+    )
+    last_name = models.CharField(
+        max_length=150,
+        verbose_name='Фамилия'
+    )
+    password = models.CharField(
+        max_length=150,
+        validators=[MinLengthValidator(5)],
+        verbose_name='Пароль'
+    )
+    role = models.CharField(
+        max_length=16,
+        choices=ROLES,
+        default='user',
+        verbose_name='Уровень доступа'
+    )
+    is_subscribed = models.BooleanField(
+        default=False,
+        verbose_name='Подписан на пользователя'
+    )
