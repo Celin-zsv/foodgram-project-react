bd7583e5ac646e46456b71a2ed75c19b9cd4a9a6
Сергей Зеленковский <svzelenkoskii@gmail.com>
2023-02-06 10:51:18 +0300

    add: 1)import User, 2)@admin.register(User); del: admin.site.register(User)

--- backend/api_foodgram/admin.py
+++ backend/api_foodgram/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
 from .models import (
     Ingredient, Tag, Recipe, IngredientRecipe, TagRecipe, Favorite, Shopping)
+from users.models import User
 
 
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ('pk', 'name', 'author', 'text', 'cooking_time')
+
+
+@admin.register(User)
+class CustomUser(UserAdmin):
+    list_filter = ('email', 'username')
 
 
 admin.site.register(Ingredient)
