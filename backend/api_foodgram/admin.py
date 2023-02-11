bf30ea97453ed07f840cb67698945b22acc41dee
Сергей Зеленковский <svzelenkoskii@gmail.com>
2023-02-11 18:16:18 +0300

    add admin.site.register for: Favorite, Subscription

--- backend/api_foodgram/admin.py
+++ backend/api_foodgram/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import (
-    Ingredient, Tag, Recipe, IngredientRecipe, TagRecipe, Favorite, Shopping)
+    Ingredient, Tag, Recipe, IngredientRecipe, TagRecipe, Favorite, Shopping, 
+    Subscription)
 from users.models import User
 
 
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ('pk', 'name', 'author', 'text', 'cooking_time')
+
+
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'recipe_id', 'user')
 
 
 @admin.register(User)
@@ -19,5 +24,6 @@ admin.site.register(Tag)
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(IngredientRecipe)
 admin.site.register(TagRecipe)
-admin.site.register(Favorite)
+admin.site.register(Favorite, FavoriteAdmin)
 admin.site.register(Shopping)
+admin.site.register(Subscription)
