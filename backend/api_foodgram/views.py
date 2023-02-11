295dc03dfb9e4130695c764e994959d2fad25a0f
Сергей Зеленковский <svzelenkoskii@gmail.com>
2023-02-11 21:35:42 +0300

    2 adds in views
    1)class RecipesWriteViewSet: @action for favorite, delete, post; @action for shopping_cart methods= post, delete; 2)class CustomUserViewSet: def get_serializer_class, @action methods=post, delete for subscribe; @action methods=get for subscriptions;

--- backend/api_foodgram/views.py
+++ backend/api_foodgram/views.py
@@ -1,19 +1,22 @@
 from django.http import HttpResponse
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from .serializers import (
     TagSerializer, IngredientSerializer, RecipesWriteSerializer,
-    RecipeReadSerializer, FavoriteSerializer
-    # CustomUserSerializer,
+    RecipeReadSerializer, FavoriteSerializer, ShoppingSerializer,
+    SubscriptionSerializer,
+    CustomUserSerializer
     # CustomCreateUserSerializer
     )
-from .models import Tag, Ingredient, Recipe, Favorite
+from .models import (
+    Tag, Ingredient, Recipe, Favorite, Shopping, Subscription)
 from users.models import User
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
-from djoser.views import UserViewSet
+# from djoser.views import UserViewSet
 from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 def zsv_page(request):
@@ -41,47 +44,100 @@ class RecipesWriteViewSet(viewsets.ModelViewSet):
             return RecipesWriteSerializer
         return RecipeReadSerializer
 
+    @action(methods=['delete', 'post'], detail=True)
+    def favorite(self, request, pk=None):
+        recipe = get_object_or_404(Recipe, pk=pk)
+        favorite_exist = Favorite.objects.filter(
+            recipe_id=pk, user=self.request.user)
 
-class CustomUserViewSet(UserViewSet):  # см. по ссылке функционал
+        if request.method == 'POST':
+            if favorite_exist.exists():
+                return Response(
+                    {'errors': 'Ошибка добавления ДУБЛЯ в избранное'},
+                    status=status.HTTP_400_BAD_REQUEST)
+            Favorite.objects.create(user=self.request.user, recipe_id=recipe)
+            serializer = FavoriteSerializer(recipe)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        if request.method == 'DELETE':  # Explicit is better than implicit
+            if not favorite_exist.exists():
+                return Response(
+                    {'errors': 'Ошибка удаления: подписка не существует'},
+                    status=status.HTTP_400_BAD_REQUEST)
+            favorite_exist.delete()
+            return Response(
+                'Рецепт успешно удален из избранного',
+                status=status.HTTP_204_NO_CONTENT)
+
+    @action(methods=['post', 'delete'], detail=True)
+    def shopping_cart(self, request, pk=None):
+        recipe = get_object_or_404(Recipe, pk=pk)
+        shopping_exist = Shopping.objects.filter(
+            recipe_id=recipe, user=self.request.user)
+
+        if request.method == 'POST':
+            if shopping_exist.exists():
+                return Response(
+                    {'error': 'Ошибка добавления ДУБЛЯ в список покупок'},
+                    status=status.HTTP_400_BAD_REQUEST)
+            Shopping.objects.create(recipe_id=recipe, user=self.request.user)
+            serializer = ShoppingSerializer(recipe)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        if request.method == 'DELETE':   # Explicit is better than implicit
+            if not shopping_exist.exists():
+                return Response(
+                    {'error': 'Ошибка удаления: покупка не существует'},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            shopping_exist.delete()
+            return Response(
+                'Рецепт успешно удален из списка покупок',
+                status=status.HTTP_204_NO_CONTENT
+            )
+
+
+# class CustomUserViewSet(UserViewSet):  # см. по ссылке функционал
 # class CustomUserViewSet(viewsets.ModelViewSet): # МНЕ: отложить - пока не разобрался чем наполнять вьюсет
+
+class CustomUserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
 
+    def get_serializer_class(self):
+        if self.action == 'subscriptions':
+            return SubscriptionSerializer
+        return CustomUserSerializer
 
-class FavoriteViewSet(viewsets.ModelViewSet):
-    serializer_class = FavoriteSerializer
-    # permission_classes = (IsAuthenticated,)    
+    @action(methods=['post', 'delete'], detail=True)
+    def subscribe(self, request, pk=None):
+        following = get_object_or_404(User, pk=pk)  # НА КОГО подписываемся
+        subscription_exist = Subscription.objects.filter(
+            following_id=following, user=self.request.user)  # КТО подписывается
 
-    def get_queryset(self):
-        print('22')
-        recipe = self.kwargs.get('recipe_id')  # 'recipe_id'- это параметр рег.выражения
-        return Favorite.objects.filter(recipe_id=recipe)
+        if request.method == 'POST':
+            if subscription_exist.exists():
+                return Response(
+                    {'error': 'Ошибка добавления ДУБЛЯ в список подписок на пользователя'},
+                    status=status.HTTP_400_BAD_REQUEST)
+            Subscription.objects.create(following_id=following, user=self.request.user)
+            serializer = SubscriptionSerializer(following)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-    def perform_create(self, serializer):
-        print('33')
-        recipe = get_object_or_404(Recipe, pk=self.kwargs.get('recipe_id'))
-        serializer.save(user=self.request.user, recipe_id=recipe)
+        if request.method == 'DELETE':   # Explicit is better than implicit
+            if not subscription_exist.exists():
+                return Response(
+                    {'error': 'Ошибка удаления: подписок на пользователя не существует'},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            subscription_exist.delete()
+            return Response(
+                'Подписка на пользователя успешно удалена из списка подписок',
+                status=status.HTTP_204_NO_CONTENT
+            )
 
-    @action(methods=['delete'], detail=False)
-    def delete(self, request):
-        print('515')
-        recipe = self.kwargs.get('recipe_id')  # 'recipe_id'- это параметр рег.выражения
-        return Favorite.objects.filter(recipe_id=recipe)
-
-    # def destroy(self, request, *args, **kwargs):
-    #     print('44')
-    #     recipe = self.kwargs.get('recipe_id')  # 'recipe_id'- это параметр рег.выражения
-    #     Favorite.objects.filter(recipe_id=recipe).delete
-
-
-    # @action(
-    #     methods=['delete'],
-    #     detail=True,  # разрешена работа с одним объектом
-    #     # permission_classes=[IsAuthenticated],
-    # )
-    # def del_favorite(self, request):
-    #     print('55')
-
-    # def get_serializer_class(self):
-    #     if self.action == 'destroy':
-    #         print('66')
-    #         return FavoriteSerializer
+    @action(methods=['get'], detail=False)
+    def subscriptions(self, request):
+        queryset_user = User.objects.filter(
+            subscriptions_following__user=self.request.user)
+        serializer = self.get_serializer(queryset_user, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
