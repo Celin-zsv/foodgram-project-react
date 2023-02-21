463484239155663421c4afe1a869bc9428ffbb09
Сергей Зеленковский <svzelenkoskii@gmail.com>
2023-02-21 19:35:58 +0300

    add recipes/admin.py (repace from app <api>)

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from recipes.models import (
+    Ingredient, Tag, Recipe, IngredientRecipe, TagRecipe, Favorite, Shopping)
 
-# Register your models here.
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'author', 'text', 'cooking_time')
+
+
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'recipe_id', 'user')
+
+
+admin.site.register(Ingredient)
+admin.site.register(Tag)
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(IngredientRecipe)
+admin.site.register(TagRecipe)
+admin.site.register(Favorite, FavoriteAdmin)
+admin.site.register(Shopping)
