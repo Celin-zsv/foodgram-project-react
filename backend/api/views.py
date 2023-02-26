d30eb4bf5ebb425c69d8c352111893a6595882ac
Сергей Зеленковский <svzelenkoskii@gmail.com>
2023-02-26 18:54:12 +0300

    change def download_shopping_cart; remove def getpdf(ingredients) (move to utils.py)

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,12 +1,5 @@
-import io
-
-from django.http import FileResponse, HttpResponse
+from django.http import HttpResponse
 from django_filters.rest_framework import DjangoFilterBackend
-from reportlab.lib.pagesizes import letter
-from reportlab.lib.units import inch
-from reportlab.pdfbase import pdfmetrics
-from reportlab.pdfbase.ttfonts import TTFont
-from reportlab.pdfgen import canvas
 from rest_framework import filters, permissions, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.generics import GenericAPIView, get_object_or_404, mixins
@@ -20,6 +13,7 @@ from users.models import Subscription, User
 from .serializers import (IngredientSerializer, RecipeReadSerializer,
                           RecipeShortSerializer, RecipesWriteSerializer,
                           SubscriptionSerializer, TagSerializer)
+from .utils import getpdf
 
 
 def zsv_page(request):
@@ -48,9 +42,16 @@ class RecipesWriteViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         if self.request.query_params.get('is_favorited'):
-            return Recipe.objects.filter(favorites__user=self.request.user)
+            return Recipe.objects.filter(
+                id__in=Favorite.objects.filter(
+                    user=self.request.user).values_list('recipe', flat=True)
+                    )
+
         if self.request.query_params.get('is_in_shopping_cart'):
-            return Recipe.objects.filter(shoppings__user=self.request.user)
+            return Recipe.objects.filter(
+                id__in=Shopping.objects.filter(
+                    user=self.request.user).values_list('recipe', flat=True)
+                    )
         return Recipe.objects.all()
 
     def perform_create(self, serializer):
@@ -65,14 +66,14 @@ class RecipesWriteViewSet(viewsets.ModelViewSet):
     def favorite(self, request, pk=None):
         recipe = get_object_or_404(Recipe, pk=pk)
         favorite_exist = Favorite.objects.filter(
-            recipe_id=pk, user=self.request.user)
+            recipe=pk, user=self.request.user)
 
         if request.method == 'POST':
             if favorite_exist.exists():
                 return Response(
                     {'errors': 'Ошибка добавления ДУБЛЯ в избранное'},
                     status=status.HTTP_400_BAD_REQUEST)
-            Favorite.objects.create(user=self.request.user, recipe_id=recipe)
+            Favorite.objects.create(user=self.request.user, recipe=recipe)
             serializer = RecipeShortSerializer(recipe)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
@@ -90,14 +91,14 @@ class RecipesWriteViewSet(viewsets.ModelViewSet):
     def shopping_cart(self, request, pk=None):
         recipe = get_object_or_404(Recipe, pk=pk)
         shopping_exist = Shopping.objects.filter(
-            recipe_id=recipe, user=self.request.user)
+            recipe=recipe, user=self.request.user)
 
         if request.method == 'POST':
             if shopping_exist.exists():
                 return Response(
                     {'error': 'Ошибка добавления ДУБЛЯ в список покупок'},
                     status=status.HTTP_400_BAD_REQUEST)
-            Shopping.objects.create(recipe_id=recipe, user=self.request.user)
+            Shopping.objects.create(recipe=recipe, user=self.request.user)
             serializer = RecipeShortSerializer(recipe)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
@@ -115,12 +116,11 @@ class RecipesWriteViewSet(viewsets.ModelViewSet):
 
     @action(methods=['get'], detail=False)
     def download_shopping_cart(self, request):
-        recepes = Recipe.objects.filter(
-            shoppings__user=self.request.user)
-        ingredients = Ingredient.objects.filter(
-            recipes__shoppings__user=self.request.user)
         ingredientrecipes = IngredientRecipe.objects.filter(
-            ingredient_id__in=ingredients, recipe_id__in=recepes)
+            recipe__in=Recipe.objects.filter(
+                id__in=Shopping.objects.filter(
+                    user=self.request.user).values_list('recipe', flat=True)
+                    ))
         return getpdf(ingredientrecipes)
 
 
@@ -132,7 +132,7 @@ class APISubscribePostDelete(mixins.CreateModelMixin, GenericAPIView):
 
         following = get_object_or_404(User, pk=kwargs['user_id'])  # on whom
         subscription_exist = Subscription.objects.filter(
-            following_id=following, user=self.request.user)  # who
+            following=following, user=self.request.user)  # who
 
         if subscription_exist.exists():
             return Response(
@@ -144,7 +144,7 @@ class APISubscribePostDelete(mixins.CreateModelMixin, GenericAPIView):
                 status=status.HTTP_400_BAD_REQUEST)
 
         Subscription.objects.create(
-            following_id=following, user=self.request.user)
+            following=following, user=self.request.user)
         serializer = SubscriptionSerializer(following)
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -152,7 +152,7 @@ class APISubscribePostDelete(mixins.CreateModelMixin, GenericAPIView):
     def delete(self, request, **kwargs):
         following = get_object_or_404(User, pk=kwargs['user_id'])  # on whom
         subscription_exist = Subscription.objects.filter(
-            following_id=following, user=self.request.user)  # who
+            following=following, user=self.request.user)  # who
 
         if not subscription_exist.exists():
             return Response(
@@ -175,45 +175,3 @@ class APISubscriptionsList(mixins.ListModelMixin, viewsets.GenericViewSet):
         queryset = User.objects.filter(
                 subscriptions_following__user=self.request.user)
         return queryset
-
-
-def getpdf(ingredients):
-    buf = io.BytesIO()
-    c = canvas.Canvas(buf, pagesize=letter, bottomup=0)
-    textobj = c.beginText()
-    textobj.setTextOrigin(inch, 1 * inch)
-    pdfmetrics.registerFont(TTFont('Arial', 'arial.ttf'))
-    textobj.setFont('Arial', 12)
-
-    lines = []
-    for ingr in ingredients:
-        if ingr.ingredient_id.name in lines:
-            nam_index = lines.index(ingr.ingredient_id.name)
-            lines[nam_index + 1] = str(int(lines[nam_index + 1]) + ingr.amount)
-        if ingr.ingredient_id.name not in lines:
-            lines.append(f'{ingr.ingredient_id.name}')
-            lines.append(f'{ingr.amount}')
-            lines.append(f'{ingr.ingredient_id.measurement_unit}')
-
-    x = 144
-    y = 14
-    textobj.moveCursor(x, 0)
-    textobj.textOut('Список покупок')
-    textobj.moveCursor(-x, 0)
-    textobj.textLine()
-    textobj.textLine()
-    v_count = 2
-    for line in lines:
-        textobj.textOut(line)
-        if v_count == 0:
-            v_count = 3
-            textobj.moveCursor(-x * 2, y)
-        else:
-            textobj.moveCursor(x, 0)
-        v_count -= 1
-
-    c.drawText(textobj)
-    c.showPage()
-    c.save()
-    buf.seek(0)
-    return FileResponse(buf, as_attachment=True, filename='shopping_list.pdf')
