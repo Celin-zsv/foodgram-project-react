c55468c944cf0d6d01932b41ec14eeaf59cbe3c3
Сергей Зеленковский <svzelenkoskii@gmail.com>
2023-02-26 19:06:47 +0300

    change fild names for Subscription

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,5 +1,6 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
-from django.core.validators import MinLengthValidator
+from django.core.validators import MinLengthValidator, RegexValidator
 from django.db import models
 
 
@@ -9,18 +10,30 @@ class User(AbstractUser):
         verbose_name='Почта'
     )
     username = models.CharField(
-        max_length=150,
+        max_length=settings.MAX_LENGTH_NAME,
         unique=True,
+        validators=[RegexValidator(
+            regex='^[a-zA-ZА-Яа-я0-9_@+.-]{2,200}$',
+            message=(
+                'Username must be Alphanumeric: Latin /Rus leters, '
+                'numbers, allowed _ @ + . - symbols, '
+                'min 2 and max 200 symbols'),
+            code='invalid_username'),
+            RegexValidator(
+                regex='[^me]',
+                message=('Is not allowed username "me"'),
+                code='invalid_username me')],
         verbose_name='Имя пользователя'
     )
     first_name = models.CharField(
-        max_length=150,
+        max_length=settings.MAX_LENGTH_NAME,
         verbose_name='Имя',
-        default='not knowen'
+        default='not known first_name'
     )
     last_name = models.CharField(
-        max_length=150,
-        verbose_name='Фамилия'
+        max_length=settings.MAX_LENGTH_NAME,
+        verbose_name='Фамилия',
+        default='not known last_name'
     )
     password = models.CharField(
         max_length=150,
@@ -32,18 +45,24 @@ class User(AbstractUser):
 
 
 class Subscription(models.Model):
-    following_id = models.ForeignKey(  # on whom
+    following = models.ForeignKey(  # on whom
         User, on_delete=models.CASCADE, related_name='subscriptions_following')
     user = models.ForeignKey(  # who
         User, on_delete=models.CASCADE, related_name='subscriptions')
 
     def __str__(self) -> str:
-        return f'{self.user}. {self.following_id}'
+        return f'{self.user}. {self.following}'
 
     class Meta:
         constraints = [
             models.UniqueConstraint(
-                fields=['following_id', 'user'],
+                fields=['following', 'user'],
                 name='unique_following_user'
+            ),
+            models.CheckConstraint(
+                name='user_equal_following',
+                check=~models.Q(
+                    user=models.F('following')
+                    )
             )
         ]
