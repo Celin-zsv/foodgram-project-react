620b5b7173b054e3829eb6d72a901ce4d62fb948
Сергей Зеленковский <svzelenkoskii@gmail.com>
2023-03-01 22:29:58 +0300

    add check for open file, del custom increment for Ingredient.id

--- backend/recipes/management/commands/load_data.py
+++ backend/recipes/management/commands/load_data.py
@@ -10,21 +10,21 @@ from recipes.models import Ingredient
 class Command(BaseCommand):
     def handle(self, *args, **options):
         path = os.path.join(settings.BASE_DIR, './', 'ingredients.json')
-        with open(path, encoding='utf-8') as f:
-            data = json.load(f)
-            model_class = Ingredient
-            load_list = []
-            v_max_id = model_class.objects.order_by('-id').first().id + 1
-            for i in data:
-                if not model_class.objects.filter(name=i['name']).exists():
-                    item_ingredient = model_class()
-                    item_ingredient.pk = v_max_id
-                    item_ingredient.name = i['name']
-                    item_ingredient.measurement_unit = i['measurement_unit']
+        try:
+            with open(path, encoding='utf-8') as upload_file:
+                data = json.load(upload_file)
+                load_list = []
+                for i in data:
+                    if not Ingredient.objects.filter(name=i['name']).exists():
+                        item_ingredient = Ingredient()
+                        item_ingredient.name = i['name']
+                        item_ingredient.measurement_unit = i[
+                            'measurement_unit']
 
-                    load_list.append(item_ingredient)
-                    v_max_id += 1
-            Ingredient.objects.bulk_create(load_list)
+                        load_list.append(item_ingredient)
+                Ingredient.objects.bulk_create(load_list)
+        except IOError as err:
+            print(f'Error with file open:: {err}')
 
         print(
-            f'loaded {len(load_list)} rec from JSON in {model_class.__name__}')
+            f'loaded {len(load_list)} rec from JSON in {Ingredient.__name__}')
