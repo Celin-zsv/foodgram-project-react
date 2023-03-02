ab62529dea21e7caf729d04c2d6cbf21e464be10
Сергей Зеленковский <svzelenkoskii@gmail.com>
2023-03-02 12:53:01 +0300

    add new file: filters

---
+++ backend/api/filters.py
@@ -0,0 +1,34 @@
+from django_filters import rest_framework
+
+from recipes.models import Favorite, Recipe, Shopping, Tag
+
+
+class RecipeFilter(rest_framework.FilterSet):
+    name = rest_framework.CharFilter(
+        field_name='name', lookup_expr='icontains')
+    author = rest_framework.NumberFilter(field_name='author')
+    tags = rest_framework.ModelMultipleChoiceFilter(
+        field_name='tags__slug',
+        to_field_name='slug',
+        queryset=Tag.objects.all())
+    is_favorited = rest_framework.BooleanFilter(
+        field_name='is_favorited',
+        method='filter_is_favorited')
+    is_in_shopping_cart = rest_framework.BooleanFilter(
+        field_name='is_in_shopping_cart',
+        method='filter_is_in_shopping_cart')
+
+    def filter_is_favorited(self, queryset, name, value):
+        return Recipe.objects.filter(
+                id__in=Favorite.objects.filter(
+                    user=self.request.user).values_list('recipe', flat=True))
+
+    def filter_is_in_shopping_cart(self, queryset, name, value):
+        return Recipe.objects.filter(
+                id__in=Shopping.objects.filter(
+                    user=self.request.user).values_list('recipe', flat=True))
+
+    class Meta:
+        model = Recipe
+        fields = [
+            'name', 'author', 'is_favorited', 'tags', 'is_in_shopping_cart']
