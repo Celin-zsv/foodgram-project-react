0b264b69c768a3b04b7ed3a6f583b65c5498ee97
Сергей Зеленковский <svzelenkoskii@gmail.com>
2023-03-04 23:36:24 +0300

    add ingredients via inlines in class RecipeAdmin

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -3,15 +3,23 @@ from recipes.models import (Favorite, Ingredient, IngredientRecipe, Recipe,
                             Shopping, Tag)
 
 
+class IngredientRecipeAdmin(admin.TabularInline):
+    model = IngredientRecipe
+    list_display = ('id', 'ingredient', 'recipe', 'amount')
+
+
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ('pk', 'name', 'author', 'text', 'cooking_time')
     search_fields = ('name', 'author', 'tags')
     list_filter = ('name', 'author', 'tags', 'pub_date')
     filter_vertical = ('tags',)
+    inlines = [
+        IngredientRecipeAdmin,
+    ]
 
 
 class FavoriteAdmin(admin.ModelAdmin):
-    list_display = ('id', 'recipe_id', 'user')
+    list_display = ('id', 'recipe', 'user')
 
 
 class IngredientAdmin(admin.ModelAdmin):
