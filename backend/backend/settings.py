bb8e5df24ca0bbcfa82aa8203b28890edfcc8653
Сергей Зеленковский <svzelenkoskii@gmail.com>
2023-03-04 23:48:56 +0300

    come back SECRET_KEY

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -18,8 +18,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = '9-p2tcuu2u^pgp!k_1@76^6v0$s#t28i5az#9ah9fknw5yu#s8'
+# SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
